2a513fe43736183d6ecf79487523f7e94dd50aff
alswnstl23 <alswnstl23@gmail.com>
2025-11-18 09:29:28 +0900

    중간저장

--- flobank-api/src/main/java/kr/co/api/flobankapi/controller/MypageController.java
+++ flobank-api/src/main/java/kr/co/api/flobankapi/controller/MypageController.java
@@ -163,9 +163,6 @@ public class MypageController {
 
     @GetMapping("/en_account_open_2")
     public String en_account_open_2(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        String custCode = user
-        mypageService.getCustInfo()
-
 
         return "mypage/en_account_open_2";
     }
