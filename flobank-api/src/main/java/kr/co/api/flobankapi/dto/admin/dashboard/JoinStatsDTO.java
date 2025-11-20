9556cc3a8dbcaaf31ce9fde76699b4117e0a2583
weasdf12 <jjyyjj118@naver.com>
2025-11-20 09:28:29 +0900

    admin_dashboard

---
+++ flobank-api/src/main/java/kr/co/api/flobankapi/dto/admin/dashboard/JoinStatsDTO.java
@@ -0,0 +1,16 @@
+package kr.co.api.flobankapi.dto.admin.dashboard;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JoinStatsDTO {
+
+    private String label;  // "2025-11-19", "2025-11", "2025-W46" 등
+    private long count;
+
+}
